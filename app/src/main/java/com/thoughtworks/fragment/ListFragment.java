5bc476a2d986b8b12782793504ecb560a819e92a
zdazx <zhangduo0227@163.com>
2020-07-21 19:48:03 +0800

    add detail in main activity and button listener

--- app/src/main/java/com/thoughtworks/fragment/ListFragment.java
+++ app/src/main/java/com/thoughtworks/fragment/ListFragment.java
@@ -4,15 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() == null) {
+            return;
+        }
+        Button androidBtn = getActivity().findViewById(R.id.android_btn);
+        androidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailFragment detailFragment = new DetailFragment();
+                if (getActivity() == null) {
+                    return;
+                }
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.detail_container, detailFragment);
+                TextView detailTextView = getActivity().findViewById(R.id.detail);
+                detailTextView.setText(R.string.android_detail);
+                transaction.commit();
+            }
+        });
+
+        Button javaBtn = getActivity().findViewById(R.id.java_btn);
+        javaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailFragment detailFragment = new DetailFragment();
+                if (getActivity() == null) {
+                    return;
+                }
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.detail_container, detailFragment);
+                TextView detailTextView = getActivity().findViewById(R.id.detail);
+                detailTextView.setText(R.string.java_detail);
+                transaction.commit();
+            }
+        });
     }
 }
--- app/src/main/java/com/thoughtworks/fragment/MainActivity.java
+++ app/src/main/java/com/thoughtworks/fragment/MainActivity.java
@@ -1,8 +1,9 @@
 package com.thoughtworks.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initDetailFragment();
+    }
+
+    private void initDetailFragment() {
+        DetailFragment detailFragment = new DetailFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.detail_container, detailFragment);
+        transaction.commit();
     }
 }
